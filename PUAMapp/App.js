17258a24279c9cd81157d44e32df61747bf36b84
Benjamin Steephenson <benjaminsteephenson@gmail.com>
2017-09-28 21:14:33 -0400

    added a map

--- PUAMapp/App.js
+++ PUAMapp/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View, Image, ScrollView } from 'react-native';
+import MapView from 'react-native-maps';
 
 export default class App extends React.Component {
   render() {
@@ -9,6 +10,15 @@ export default class App extends React.Component {
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
         <Image style = {{ height: 100, width: 100 }} source = {{url: "http://www.asseenontvvideo.com/wp-content/uploads/2013/10/BillyMays-trimmed1.jpg"}} />
+        <MapView
+          style = {{ height: 300, width: 300 }}
+          initialRegion={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        />
       </View>
     );
   }
