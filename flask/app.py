9f7f329f9885c9006865259f7d5f2b2974141371
chris-anderson67 <chrisja67@gmail.com>
2017-10-03 21:26:30 -0400

    added requirements.txt

--- flask/app.py
+++ flask/app.py
@@ -1,6 +1,15 @@
 from flask import Flask
 app = Flask(__name__)
 
+from flask.ext.wtf import Form
+from wtforms.fields import StringField, BooleanField
+from wtforms.validators import DataRequired
+
+class FirePut(Form):
+    title = StringField('title', validators=[DataRequired()])
+    year = StringField('year', validators=[DataRequired()])
+    rating = StringField('rating', validators=[DataRequired()])
+
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
