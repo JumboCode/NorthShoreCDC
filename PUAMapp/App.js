f9dbe6be67e78711c8c5ed3bd98fc302f8a11ba1
Benjamin Steephenson <benjaminsteephenson@gmail.com>
2017-10-05 20:44:24 -0400

    attempting to add navigation

--- PUAMapp/App.js
+++ PUAMapp/App.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import { StyleSheet, Text, View, Image, ScrollView } from 'react-native';
+import {StackNavigator} from 'react-navigation'
 import MapView from 'react-native-maps';
 
-export default class App extends React.Component {
+// TODO turn this into a navigation thingy
+
+const App = StackNavigator({
+  Welcome: {screen: WelcomeScreen},
+  MapScreen: {screen: MapScreen}
+})
+
+export default App;
+
+
+class WelcomeScreen extends React.Component {
   render() {
     return (
       <View style={styles.container}>
@@ -10,18 +21,27 @@ export default class App extends React.Component {
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
         <Image style = {{ height: 100, width: 100 }} source = {{url: "http://www.asseenontvvideo.com/wp-content/uploads/2013/10/BillyMays-trimmed1.jpg"}} />
-        <MapView
-          style = {{ height: 300, width: 300 }}
-          initialRegion={{
-            latitude: 37.78825,
-            longitude: -122.4324,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421,
-          }}
-        />
       </View>
     );
   }
+}
+
+class MapScreen extends React.Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <MapView
+                  style = {{ height: 300, width: 300 }}
+                  initialRegion={{
+                    latitude: 37.78825,
+                    longitude: -122.4324,
+                    latitudeDelta: 0.0922,
+                    longitudeDelta: 0.0421,
+                  }}
+                />
+            </View>
+        )
+    }
 }
 
 const styles = StyleSheet.create({
