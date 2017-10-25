91aa84142eb9d47bc651d1d25240d88d1fea4702
Suruchi Devanahalli <suruchidevanahalli@Suruchis-MacBook-Pro.local>
2017-10-25 00:37:12 -0400

    incorporated redux, made an action, made a reducer

---
+++ PUAMapp/firebase.js
@@ -0,0 +1,13 @@
+import * as firebase from 'firebase';
+
+// should go in a secret file
+const config = {
+    apiKey: "AIzaSyDbWmnZG4J3YM1qPIwrSpMM60eLM6TPHsA",
+    authDomain: "puntourbanart.firebaseapp.com",
+    databaseURL: "https://puntourbanart.firebaseio.com",
+    storageBucket: "puntourbanart.appspot.com",
+    messagingSenderId: "873188637889"
+};
+firebase.initializeApp(config);
+
+export default firebase;
