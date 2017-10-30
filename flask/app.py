a0b715dcf27f98542a6677e47246530e5b2b83d0
Brendan Fahey <bmfahey97@gmail.com>
2017-10-29 20:59:16 -0400

    validators work now

--- flask/app.py
+++ flask/app.py
@@ -3,7 +3,7 @@ from firebase import firebase
 import os
 from flask_wtf import FlaskForm as Form
 from wtforms.fields import StringField, BooleanField
-from wtforms.validators import DataRequired
+from wtforms.validators import *
 from flask_wtf.csrf import CSRFProtect
 
 firebase_path = os.environ.get('FIREBASE_PATH')
@@ -13,17 +13,13 @@ app = Flask(__name__)
 app.secret_key = os.environ.get('APP_KEY')
 
 class FirePut(Form):
-    photo = StringField('Photo', validators=[DataRequired()])
-    lat = StringField('Lat', validators=[DataRequired()])
-    longitude = StringField('Long', validators=[DataRequired()])
-    streetaddress = StringField('Street_Address', validators=[DataRequired()])
-    city = StringField('City', validators=[DataRequired()])
-    state = StringField('State', validators=[DataRequired()])
-    zipcode = StringField('Zipcode', validators=[DataRequired()])
+    photo = StringField('Photo', validators=[DataRequired(), URL(require_tld=True, message=None)])
+    lat = StringField('Lat', validators=[DataRequired(), NumberRange(min="40.0", max = "43.0")])
+    longitude = StringField('Long', validators=[DataRequired(), NumberRange(min="-71.0", max = "-69.0")])
     artist = StringField('Artist', validators=[DataRequired()])
     title = StringField('Title', validators=[DataRequired()])
-    month = StringField('Month', validators=[DataRequired()])
-    year = StringField('Year', validators=[DataRequired()])
+    month = StringField('Month', validators=[DataRequired(), AnyOf(["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"])])
+    year = StringField('Year', validators=[DataRequired(), NumberRange(min="1980", max = "3000")])
 
 count = 0
 
@@ -34,9 +30,7 @@ def fireput():
         global count
         count += 1
         putData = { 'Photo' : form.photo.data, 'Lat' : form.lat.data,
-        			'Long' : form.longitude.data, 'Street_Address' : form.streetaddress.data,
-        			'City' : form.city.data, 'State' : form.state.data,
-        			'Zipcode' : form.zipcode.data, 'Artist' : form.artist.data,
+        			'Long' : form.longitude.data, 'Artist' : form.artist.data,
         			'Title' : form.title.data, 'Month' : form.month.data,
         			'Year' : form.year.data }
         firebase.put('/murals', putData['Title'], putData)
