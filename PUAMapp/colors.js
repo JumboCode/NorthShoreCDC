64d330333531972b8a4b625ae8d7f7fde4ca4909
Lillianna <llevonick@gmail.com>
2017-11-04 17:03:15 -0400

    Got the buttons to be the right color & fixed the margins

--- PUAMapp/colors.js
+++ PUAMapp/colors.js
@@ -1,4 +1,3 @@
-  //export default {"832161" as lightpurple, "602A5F" as darkpurple, "DD3088" as pink};
   export const lightpurple = "#832161";
   export const darkpurple = "#602A5F";
   export const pink = "#DD3088";
