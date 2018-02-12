728345569451fd8a3db8fcb2c507a9d2cf1e1b85
Benjamin Steephenson <benjaminsteephenson@gmail.com>
2018-02-11 21:44:08 -0500

    The user should be prompted with our specific reason for needing user location.
    NOTE: It's not possible to test this change without running `exp build`, see https://docs.expo.io/versions/latest/guides/app-stores.html#system-permissions-dialogs-on-ios

--- PUAMapp/ExplorePage.js
+++ PUAMapp/ExplorePage.js
@@ -61,6 +61,7 @@ export default class ExplorePage extends React.Component {
             <View style = {{flex: 1}}>
             <StatusBar barStyle = { Platform.OS === 'ios' ? "dark-content" : "light-content"}/>
             <MapView
+              showsUserLocation = {true}
               style = {{flex: 1 }}
               region = {{
                 latitude: 42.518217,
